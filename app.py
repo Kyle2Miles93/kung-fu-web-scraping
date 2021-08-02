937a8cc23374960f9de1fb5bed5929380ca99154
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2021-08-02 22:31:53 +0900

    refactored mars things for this adapted project.

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import Scraping
+import scraping
 
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/kungFu"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    sifus = mongo.db.sifus.find_one()
+    return render_template("index.html", sifus=sifus)
 
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = Scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
+    sifus = mongo.db.sifus
+    sifus_data = scraping.scrape_all()
+    sifus.update({}, sifus_data, upsert=True)
     print("Scraping Sucessful!")
     return redirect('/', code=302) 
 
